f5be81e848153ef22adc9616349120a3528a5c88
Mega-Mew <matt6.lambert@laposte.net>
2017-09-29 10:37:38 +0200

    Add comment in dream patcher

--- Subforms/DreamPatcher.Designer.cs
+++ Subforms/DreamPatcher.Designer.cs
@@ -28,22 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DreamPatcher));
             this.TB_FilePath = new System.Windows.Forms.TextBox();
             this.B_PatchFile = new System.Windows.Forms.Button();
             this.B_OpenFile = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TB_FilePath
             // 
             this.TB_FilePath.Location = new System.Drawing.Point(12, 24);
             this.TB_FilePath.Name = "TB_FilePath";
-            this.TB_FilePath.Size = new System.Drawing.Size(397, 20);
+            this.TB_FilePath.Size = new System.Drawing.Size(461, 20);
             this.TB_FilePath.TabIndex = 0;
             // 
             // B_PatchFile
             // 
-            this.B_PatchFile.Location = new System.Drawing.Point(209, 50);
+            this.B_PatchFile.Location = new System.Drawing.Point(249, 50);
             this.B_PatchFile.Name = "B_PatchFile";
             this.B_PatchFile.Size = new System.Drawing.Size(75, 23);
             this.B_PatchFile.TabIndex = 2;
@@ -53,7 +55,7 @@
             // 
             // B_OpenFile
             // 
-            this.B_OpenFile.Location = new System.Drawing.Point(128, 50);
+            this.B_OpenFile.Location = new System.Drawing.Point(168, 50);
             this.B_OpenFile.Name = "B_OpenFile";
             this.B_OpenFile.Size = new System.Drawing.Size(75, 23);
             this.B_OpenFile.TabIndex = 3;
@@ -70,11 +72,21 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "garden_plus.dat input:";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(453, 91);
+            this.label2.TabIndex = 5;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
             // DreamPatcher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 81);
+            this.ClientSize = new System.Drawing.Size(478, 187);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.B_OpenFile);
             this.Controls.Add(this.B_PatchFile);
@@ -94,5 +106,6 @@
         private System.Windows.Forms.Button B_PatchFile;
         private System.Windows.Forms.Button B_OpenFile;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
